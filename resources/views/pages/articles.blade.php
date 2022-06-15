8d38d9dc62c21ad6d4f4105da706082be9fdf2cc
jocelinkisenga <kisengajocelin7@gmail.com>
2022-06-15 23:22:44 +0200

    adding route for categorie controller

---
+++ resources/views/pages/articles.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+	<div class="row">
+	  	<form action="{{route('store-article')}}" method="POST">
+	  	@csrf 
+	  		
+	  			<div class="form-group">
+	  				<label for="disabledTextInput">enter the categorie name</label>
+	  				<input type="text" class="form-control" name="title" placeholder="categorie name">
+	  			</div>
+	  			
+	  			
+	  			<button type="submit" class="btn btn-primary">Submit</button>
+	  		
+	  	</form>
+	</div>
+</div>
+
+@endsection
