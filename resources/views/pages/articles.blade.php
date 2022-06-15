add4e3408d2c3d0392c5311f2518f33ef473c0d8
jocelinkisenga <kisengajocelin7@gmail.com>
2022-06-15 23:49:35 +0200

    adding route for categorie controller

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -10,6 +10,11 @@
 	  				<label for="disabledTextInput">enter the categorie name</label>
 	  				<input type="text" class="form-control" name="title" placeholder="categorie name">
 	  			</div>
+	  			<div class="form-group">
+	  				<label for="disabledTextInput">enter the categorie name</label>
+	  				<input type="text" class="form-control" name="title" placeholder="categorie name">
+	  			</div>
+
 	  			<div class="form-group col-md-auto">
 	  				<textarea id="myTextarea"></textarea>
 	  			</div>
